1bf411caad6c766878155d4a1f394f6f060fa4d0
David Alderson <dsa027@gmail.com>
2018-03-26 18:23:39 -0700

    create bro science trivia

---
+++ custom/question.js
@@ -0,0 +1,205 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'Which body part is the quote legs of the upper body unquote?': [
+                'Back',
+                'Legs',
+                'Arms',
+                'Chest',
+            ],
+        },
+        {
+            'Which workout day should be devoted to arms?': [
+                'All of them',
+                'Moday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+            ],
+        },
+        {
+            'Why do bros walk around the gym like they can\'t put their arms to their sides?': [
+                'Invisible Latisimus Dorsi Syndrome',
+                'Their latisimus dorsi are too big',
+                'Their biceps are too big',
+                'Their tricepts are too big',
+                'Their chest is too big',
+            ],
+        },
+        {
+            'How do you know bro science works?': [
+                'Most people avoid you at the gym',
+                'Guys complement you',
+                'Girls complement you',
+                'You just never know',
+            ],
+        },
+        {
+            'How much protein is too much protein?': [
+                'There\'s no such thing',
+                '1 gram per pound of weight is typically the maximum for bodybuilding',
+                'I\'m supposed to eat protein?',
+                'You don\'t need protein because your muscles are made of protein',
+            ],
+        },
+        {
+            'What is the best motivator at the gym?': [
+                'Being loud, banging weights, and cat calling women under your breath',
+                'People telling you your biceps are gross',
+                'Other gym-goers laughing at your little legs',
+                'A woman accidentally looks at you',
+            ],
+        },
+        {
+            'What is the sweet spot in terms of meals per day?': [
+                '8',
+                '7',
+                '6',
+                '3 or 4',
+            ],
+        },
+        {
+            'In what condition should you do cardio?': [
+                'Fasted',
+                'With some carbs in your stomach',
+                'Cardio?',
+                'Twisted',
+            ],
+        },
+        {
+            'Can I eat carbs at night?': [
+                'Are you freaking crazy?',
+                'Yes, just not to excess',
+                'Try to avoid within an hour of bedtime',
+                'Yes, complex carbs are fine',
+            ],
+        },
+        {
+            'Where is the best place in the gym to do bicep curls?': [
+                'Anywhere, but curling in the squat rack is best',
+                'Against a wall',
+                'At a bicep curl machine',
+                'Not in the freaking squat rack!',
+            ],
+        },
+        {
+            'What is the max amount of time I should be stretching?': [
+                'Zero time',
+                'Warm up muscles, don\'t stretch before lifting, only after',
+                'Warm up with dynamic stretching',
+                'Just make sure you stretch for a bit after your workout',
+            ],
+        },
+        {
+            'Is form important?': [
+                'It\'s not about form, it\'s how much weight you can move!',
+                'Yes, unless you want to eventually injure yourself',
+                'Only on the first 2 reps',
+                'Always',
+            ],
+        },
+        {
+            'What\'s the best exerciese for legs?': [
+                'I didn\'t hear that. Still can\'t hear you. Be quiet!',
+                'Most people believe squats are the best',
+                'Lunges are great, too',
+                'Smith machine lunges and squats',
+            ],
+        },
+        {
+            'I need an excuse today for not doing legs, please?': [
+                'It\'s arm day!',
+                'There is no excuse. Do legs!',
+                'Lower back pain and knee pain',
+                'Legs are pretty close to half your body. Do legs',
+            ],
+        },
+        {
+            'If your girlfriend asks you to join her for her leg workout today, what do you do?': [
+                'Break up, immediately!',
+                'Do the leg workout with her',
+                'Laugh',
+                'Cry',
+            ],
+        },
+        {
+            'Should I use clips on heavy lifts?': [
+                'Are you a girl?',
+                'Yeah, you should. You don\'t want to injure yourself or others',
+                'Use clips on all lifts',
+                'Only if you aren\'t perfect',
+            ],
+        },
+        {
+            'What is creatine?': [
+                'A steroid!',
+                'It helps replenish ATP, which is what your body uses to create energy',
+                'One of the two best bodybuilding supplements, the first being protein',
+                'The devil!',
+            ],
+        },
+        {
+            'Who never lies to you?': [
+                'The iron!',
+                'No one',
+                'My mom',
+                'No one ever tells me the truth',
+            ],
+        },
+        {
+            'Should I hold my breath or breathe during a set?': [
+                'Whatever you do, don\'t breathe, your body will think you\'re doing cardio',
+                'Breathe normally',
+                'In on the eccentric, slowly out on the concentric ',
+                'Breathe on every rep and only hold your breath if you\'re doing heavy squats or deadlifts',
+            ],
+        },
+        {
+            'What\'s the best exercise to get rid of stomach fat?': [
+                'Millions of ab reps',
+                'Abs are made in the kitchen',
+                'There\'s no such thing as true spot reduction, so just workout and eat right!',
+                'Liposuction',
+            ],
+        },
+        {
+            'How do I turn fat into muscle?': [
+                'Bicep workouts!',
+                'There\'s no such thing',
+                'Lose fat, gain muscle',
+                'Chest workouts!',
+            ],
+        },
+        {
+            'Should women lift weights?': [
+                'Not unless they want to get bulky and look like a power lifter',
+                'Yes! Absolutely! Women rarely bulk up when lifting weights',
+                'As long as they aren\'t doing steroids, but then again, if they are, they want to bulk',
+                'Serious, consistent lifting makes everyone look sexier',
+            ],
+        },
+        {
+            'What\'s the best way to take protein powder?': [
+                'Dry man, dry',
+                'With plenty of water',
+                'With a scoop',
+                'Upon waking, before a workout, after a workout with carbs, and before bed',
+            ],
+        },
+        {
+            'Why is chicken the best protein?': [
+                'Because chickens evolved from dinosaurs and dinosaurs are true bad asses',
+                'Light meat is low calorie, low fat, and has a high protein concentration',
+                'Because it\'s dark and light!',
+                'Because they walk funny and sometimes drown looking up at the rain',
+            ],
+        },
+    ],
+};
